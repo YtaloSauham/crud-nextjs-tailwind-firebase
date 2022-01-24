546dd41954e1d987a47d74857ea8545e73654218
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-01-24 18:04:50 -0300

    Firebase integrado

---
+++ src/backend/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+
+
+if(!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey:process.env. NEXT_PUBLIC_FIREBASE_API_KEY,
+        autDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID 
+    })
+}
