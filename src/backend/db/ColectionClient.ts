976f7cf164e3880c291a8fb8c1b9cc639727ad11
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-01-27 09:58:24 -0300

    Cliente repositorio habilitado

--- src/backend/db/ColectionClient.ts
+++ src/backend/db/ColectionClient.ts
@@ -1,18 +1,34 @@
 import Client from "../../core/Client";
 import ClientRepository from "../../core/ClientRepository";
-
+import firebase from "../firebase";
 export default class ColectionClient implements ClientRepository {
+
+
+    conversor = {
+        toFirestore( client : Client) {
+            return {
+                name: client.name,
+                age: client.age,
+            }
+        },
+        fromFiresotre(snapshot: firebase.firestore.QueryDocumentShapsot , options: firebase.firesotre.SnapshotOptions) : Client{
+
+            const dados = snapshot.data(options)
+            return new Client(dados.name, dados.age,snapshot.id)
+
+        }
+    }
 
 
     async save(client: Client): Promise<Client> {
         return null
     }
 
-    async remove(client: Client): Promise<Client> {
+    async remove(client: Client): Promise<void> {
         return null
     }
 
-    async getAll(): Promise<Client> {
+    async getAll(): Promise<Client[]> {
         return null
     }
     
